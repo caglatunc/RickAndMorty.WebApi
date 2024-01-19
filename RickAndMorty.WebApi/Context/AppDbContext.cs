f810065bf27ca993eed01600ac33b27c676b9201
caglatunc <caglatunc03@gmail.com>
2024-01-19 23:55:02 +0300

    API request.

--- RickAndMorty.WebApi/Context/AppDbContext.cs
+++ RickAndMorty.WebApi/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RickAndMorty.WebApi.Models;
 using System.Reflection;
 
 namespace RickAndMorty.WebApi.Context;
@@ -9,8 +10,23 @@ public sealed class AppDbContext : DbContext
     {
     }
 
+    public DbSet<Episode> Episodes { get; set; }
+    public DbSet<EpisodeCharacter> EpisodeCharacters { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Origin> Origins { get; set; }
+    public DbSet<Character> Characters { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<EpisodeCharacter>()
+            .HasKey(x=> new { x.EpisodeId, x.CharacterId });//Composite key
+
+        modelBuilder.Entity<Episode>().HasMany(p => p.EpisodeCharacters).WithOne();
+
+        modelBuilder.Entity<EpisodeCharacter>().HasOne<Episode>().WithMany(p => p.EpisodeCharacters).HasForeignKey(p => p.EpisodeId);
+
+        modelBuilder.Entity<EpisodeCharacter>().HasOne(p=>p.Character).WithMany().HasForeignKey(p => p.CharacterId);
+
+        modelBuilder.Entity<Character>().HasMany<EpisodeCharacter>().WithOne(p => p.Character).OnDelete(DeleteBehavior.NoAction);
+
     }
 }
---
+++ RickAndMorty.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RickAndMorty.WebApi.Context;
+using RickAndMorty.WebApi.DTOs;
+
+namespace RickAndMorty.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController(HttpClient httpClient) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsingRickAndMortyAPI()
+    {
+        var response = await httpClient.GetAsync("https://rickandmortyapi.com/api/episode");//Api isteği attım
+        EpisodeDto? episodeDto = await response.Content.ReadFromJsonAsync<EpisodeDto>();//Gelen response'u EpisodeDto'ya çevirdim
+
+        return Ok(episodeDto);
+    }
+}
